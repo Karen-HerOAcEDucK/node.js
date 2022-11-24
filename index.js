109ea238e0fd7f917397e39e03e3f540d25c29d6
Karen Letícia Alexandre <heroace44@gmail.com>
2022-11-24 19:41:43 -0300

    Utilizando forma assincrona de codigo, porem na utilização de async e await, para a leitura do arquivo

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
+import fs from 'fs'
 import chalk from 'chalk'
+import { waitForDebugger } from 'inspector'
 
-console.log(chalk.blue('Hello world!'));
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretorio'))
+}
 
-console.log('olá mundo');
+// Função sincrona que irá realizar o que é esperado porém podendo gerar problemas com a demora na execução em algum ponto da mesma
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8'
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if(erro){
+//             trataErro(erro)
+//         }
+//         console.log(chalk.green(texto))
+//     })
+// }
+
+// Utilizando codigo assincrono, utilizando as promisses do javascript juntamente com o then e o catch
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8'
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro)
+// }
+
+// Utilizando codigo asssincrono porém, utilizando a forma mais moderna do javascript o async e o await
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const encoding = 'utf-8'
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto));
+    } catch(error) {
+        trataErro(error)
+    }
+}
+
+pegaArquivo('./arquivos/texto.md')
+pegaArquivo('./arquivos/')
